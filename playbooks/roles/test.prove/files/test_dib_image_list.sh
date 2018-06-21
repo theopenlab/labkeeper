5f8328414b5da191ce7d2f7a95145621340b70db
liusheng <liusheng@huawei.com>
2018-06-21 14:40:47 +0800

    update

--- playbooks/roles/test.prove/files/test_dib_image_list.sh
+++ playbooks/roles/test.prove/files/test_dib_image_list.sh
@@ -15,6 +15,4 @@
 IMAGE=$1
 STATUS=$2
 
-VENV=/opt/venv/nodepool
-
-$VENV/bin/nodepool dib-image-list | awk '{print $2,$10;}' | grep $IMAGE | grep $STATUS
+nodepool dib-image-list | awk '{print $2,$10;}' | grep $IMAGE | grep $STATUS
