af6cefe2159959a926124d518ce5091d694b04be
liusheng <liusheng@huawei.com>
2018-06-25 20:08:19 +0800

    update

---
+++ config/zuul/mysql_secure_installation.sql
@@ -0,0 +1,10 @@
+UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+CREATE USER 'zuul'@'localhost' IDENTIFIED BY 'zuul';
+CREATE DATABASE IF NOT EXISTS zuul;
+GRANT ALL PRIVILEGES ON zuul.* TO 'zuul'@'localhost' IDENTIFIED BY 'zuul';
+FLUSH PRIVILEGES;
