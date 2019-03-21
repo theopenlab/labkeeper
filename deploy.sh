145781c23b54d83b0384e8c82f9f40f8db3e66cc
liu-sheng <liusheng2048@gmail.com>
2019-03-21 11:13:00 +0800

    support HA deployment

--- deploy.sh
+++ deploy.sh
@@ -25,5 +25,5 @@ fi
 
 sudo apt update -y
 sudo apt install python python-pip python3 python3-pip kpartx qemu-utils curl python-yaml debootstrap libffi-dev libssl-dev -y
-sudo pip install ansible
+sudo pip install -U pip setuptools wheel virtualenv ansible
 ansible-playbook playbooks/site.yaml -i inventory/${DPLOY_TYPE}.yaml
