e0b49e9c96e2a257ca2377b9bbc8fdb5a42df059
liu-sheng <liusheng2048@gmail.com>
2019-03-22 10:02:49 +0800

    fix nodepool-builder dib elements setup

--- deploy.sh
+++ deploy.sh
@@ -17,12 +17,6 @@ fi
 export ANSIBLE_REMOTE_USER=${ANSIBLE_REMOTE_USER:-ubuntu}
 export DPLOY_TYPE=${DPLOY_TYPE:-allinone}
 
-echo "Checking key and user access, 'ubuntu' user as default, you can set 'ANSIBLE_REMOTE_USER' to override!"
-if ! ssh -o "StrictHostKeyChecking no" -i openlab.pem ${ANSIBLE_REMOTE_USER}@localhost 'pwd'; then
-    echo "Please ensure you have created correct 'openlab.pem' and switched to the user which can access with this key!"
-    exit 1
-fi
-
 sudo apt update -y
 sudo apt install python python-pip python3 python3-pip kpartx qemu-utils curl python-yaml debootstrap libffi-dev libssl-dev -y
 sudo pip install -U pip setuptools wheel virtualenv ansible
