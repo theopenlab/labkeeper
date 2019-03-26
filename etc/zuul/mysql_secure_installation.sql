18ffef0bc7355b70ef643f0233f5011c52ac459a
liu-sheng <liusheng2048@gmail.com>
2019-03-26 14:49:35 +0800

    debug mysql configuration

--- etc/zuul/mysql_secure_installation.sql
+++ etc/zuul/mysql_secure_installation.sql
@@ -4,7 +4,7 @@ DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 FLUSH PRIVILEGES;
-CREATE USER 'zuul'@'localhost' IDENTIFIED BY 'zuul';
+CREATE USER IF NOT EXISTS 'zuul'@'localhost' IDENTIFIED BY 'zuul';
 CREATE DATABASE IF NOT EXISTS zuul;
 GRANT ALL PRIVILEGES ON zuul.* TO 'zuul'@'localhost' IDENTIFIED BY 'zuul';
 FLUSH PRIVILEGES;
