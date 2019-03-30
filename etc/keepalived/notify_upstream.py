0a12ecadd1a14941a3a32f30d0e7671aebf4fb90
ZhaoBo <zhaobo6@huawei.com>
2019-03-30 10:42:13 +0800

    Add keepalived scripts
    For now, we test with a sample github account, as changing webhook is
    very dangerous for online ENV.

---
+++ etc/keepalived/notify_upstream.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import argparse
+
+from github import Github
+
+REPO_NAME = 'moo-ai/moo-ai.github.io'
+
+
+def notify_issue(args):
+    g = Github(args.username, args.password)
+    repo = g.get_repo(REPO_NAME)
+    repo.create_issue(
+        title="WARNING: The online openlab deployment <%s> has Down, "
+              "Please recovery asap!" % args.env_role,
+        body="")
+
+
+def main(args):
+    notify_issue(args)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Github tools',
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('--username',  metavar='<username>', required=True,
+                        help="Github User Name")
+    parser.add_argument('--password', metavar='<password>', required=True,
+                        help="The PassWord of Github User")
+    parser.add_argument('--env-role', metavar='<env_role>', required=True,
+                        help="The PassWord of Github User")
+    args = parser.parse_args()
+    main(args)
