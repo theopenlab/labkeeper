71555029eec148e4011badf3fb5544b24ea9584f
huangtianhua <huangtianhua@huawei.com>
2019-04-25 14:25:17 +0800

    Delete branch if config files not changed (#56)
    To delete branch anyway.
    
    Related-Bug: theopenlab/openlab#84

--- labsync/nodepool_files_sync.sh
+++ labsync/nodepool_files_sync.sh
@@ -83,6 +83,7 @@ if [[ $is_modified ]];then
     export GITHUB_TOKEN=${github_token}
     hub pull-request -m "${message}"
     echo "Create pull request to theopenlab/labkeeper success!"
-    hub checkout master
-    hub branch -D ${branch_name}
 fi
+
+hub checkout master
+hub branch -D ${branch_name}
