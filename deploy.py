d8fdb4ea10a68e500d3195996ff617c616e6324c
wangxiyuan <wangxiyuan@huawei.com>
2019-05-23 10:00:56 +0800

    Update deploy script (#87)
    1. Merge some parameters to into one `action` to make cli clearner.
    2. Fix a bud that `new-slave` action doesn't update master cluster.
    2. Move HA deploy step the last so that everything is OK before
       start health checker.
    3. Separate mysql install and ha config steps to different file.
    
    Related-Bug: theopenlab/openlab#218

--- deploy.py
+++ deploy.py
@@ -13,22 +13,25 @@ def add_cli_args():
                                  "openlab-ha"],
                         help='OpenLab deployment type',
                         )
+    parser.add_argument('--action',
+                        choices=["deploy", "new-slave", "switch-role",
+                                 "show-graph", "show-ip"],
+                        default="deploy",
+                        help="The action that labkeeper supports. Default "
+                             "value is 'deploy'.\n"
+                             "'deploy': create a new OpenLab CI environment.\n"
+                             "'new-slave': create new slave nodes base on "
+                             "current master nodes.\n"
+                             "'switch-role': switch the master/slave role of "
+                             "current deployment. This will re-config the "
+                             "existing OpenLab environment.\n"
+                             "'show-graph': show the hosts graph of ansible "
+                             "inventory. It can be used with --with-vars to "
+                             "show more detail.\n"
+                             "'show-ip': show hosts ip address.\n")
     parser.add_argument('-u', '--user',
                         help='the Ansible remote user performing deployment, '
                              'default is "ubuntu" configured in ansible.cfg',
-                        )
-    parser.add_argument('--switch-role',
-                        action='store_true',
-                        help='switch the master/slave role of current deployment, '
-                             'this will re-config an existing OpenLab environment to '
-                             'switch the "master/slave" role of hosts',
-                        )
-    parser.add_argument('--new-slave',
-                        action='store_true',
-                        help='deploy the slave part of a CI cluster freshly, you need '
-                             'to specify the new slave hosts IP addresses with "--new-ip" '
-                             'argument, for example: '
-                             '--new-ip zuul02=192.168.5.5 --new-ip nodepool02=192.168.6.6',
                         )
     parser.add_argument('-e', '--extra-vars',
                         metavar="<key=value>",
@@ -36,30 +39,20 @@ def add_cli_args():
                         default=[],
                         help='extra vars passed to Ansible command',
                         )
-    parser.add_argument('--new-ip',
+    parser.add_argument('--host-ip',
                         metavar="<key=value>",
                         action='append',
-                        help='specify new ip address for current inventory, this argument '
-                             'need to use with key-value pairs combined with "=", the key '
-                             'must be the host name defined in the inventory yaml files, '
-                             'e.g. zuul01, nodepool02, allinone01.',
+                        help='override new ip address for current inventory, '
+                             'this argument need to use with key-value pairs '
+                             'combined with "=", the key must be the host name'
+                             ' defined in the inventory yaml files, e.g. '
+                             'zuul01, nodepool02, allinone01.',
                         )
     parser.add_argument('--with-vars',
                         action='store_true',
-                        help='show the hosts graph of ansible inventory with vars, specify with '
-                             'key-value pairs combined with "="',
-                        )
-    parser.add_argument('--graph',
-                        action='store_true',
-                        help='show the hosts graph of ansible inventory',
-                        )
-    parser.add_argument('--show-old-ips',
-                        action='store_true',
-                        help='show the old hosts ip address',
-                        )
-    parser.add_argument('--show-new-ips',
-                        action='store_true',
-                        help='show the new hosts ip address',
+                        help='show the hosts graph of ansible inventory with '
+                             'vars. It must be used together with --action '
+                             'show-graph.'
                         )
     return parser
 
@@ -67,50 +60,47 @@ def add_cli_args():
 def main():
     parsed_args = add_cli_args().parse_args()
     os.environ['OL_TYPE'] = parsed_args.type
-    cmd = ['ansible-playbook', '-i', 'inventory/inventory.py', 'playbooks/site.yaml']
+    cmd = []
 
-    if parsed_args.user:
-        cmd += ['-u', parsed_args.user]
+    if parsed_args.action == 'deploy':
+        cmd = ['ansible-playbook', '-i', 'inventory/inventory.py',
+               'playbooks/site.yaml']
+    elif parsed_args.action == 'new-slave':
+        cmd = ['ansible-playbook', '-i', 'inventory/inventory.py',
+               'playbooks/site.yaml', '-l', '*-slave']
+    elif parsed_args.action == 'switch-role':
+        os.environ['OL_SWITCH_MASTER_SLAVE'] = True
+        cmd = ['ansible-playbook', '-i', 'inventory/inventory.py',
+               'playbooks/switch_role.yaml']
+    elif parsed_args.action == 'show-graph':
+        cmd = ['ansible-inventory', '-i', 'inventory/inventory.py', '--graph']
+        if parsed_args.with_vars:
+            cmd.append('--vars')
+    elif parsed_args.action == 'show-ip':
+        cmd = ['python', 'inventory/inventory.py', '--show-ip']
 
-    if parsed_args.new_ip:
-        specified_ips = dict([(d.partition('=')[::2]) for d in parsed_args.new_ip])
+    if parsed_args.host_ip:
+        specified_ips = dict([(d.partition('=')[::2])
+                              for d in parsed_args.new_ip])
         for host, ip in specified_ips.items():
             os.environ['OL_%s_IP' % host.upper()] = ip
 
-    if parsed_args.new_slave:
-        if not parsed_args.new_ip:
-            sys.exit("ERROR: Must specify the new slave hosts IPs with --new-ip")
-        os.environ['OL_SWITCH_MASTER_SLAVE'] = True
-        cmd += ['-l', '*-slave']
-        parsed_args.extra_vars.append('config_new_slave=true')
+    if parsed_args.user and cmd[0] == 'ansible-playbook':
+        cmd += ['-u', parsed_args.user]
 
     if parsed_args.extra_vars:
         cmd += ['-e', ' '.join(parsed_args.extra_vars)]
 
-    if parsed_args.switch_role:
-        os.environ['OL_SWITCH_MASTER_SLAVE'] = True
-        cmd = ['ansible-playbook', '-i', 'inventory/inventory.py', 'playbooks/conf-new-slave.yaml']
-
-    if parsed_args.graph:
-        cmd = ['ansible-inventory', '-i', 'inventory/inventory.py', '--graph']
-        if parsed_args.with_vars:
-            cmd.append('--vars')
-
-    if parsed_args.with_vars and not parsed_args.graph:
-        sys.exit("ERROR: --with-vars must work with --graph")
-
-    if parsed_args.show_new_ips:
-        cmd = ['python', 'inventory/inventory.py', '--new-host-ips']
-
-    if parsed_args.show_old_ips:
-        cmd = ['python', 'inventory/inventory.py', '--old-host-ips']
-
-    ol_env_msg = '\n'.join(['%s=%s' % (k, os.environ[k]) for k in os.environ if k.startswith('OL_')])
+    ol_env_msg = '\n'.join(['%s=%s' % (k, os.environ[k]) for k in os.environ
+                            if k.startswith('OL_')])
     print("OpenLab deployment ENV:\n%s" % ol_env_msg)
     print('Ansible command:\n%s' % ' '.join(cmd))
     print("*" * 100)
-
     subprocess.call(cmd)
+
+    if parsed_args.action == 'new-slave':
+        subprocess.call(['ansible-playbook', '-i', 'inventory/inventory.py',
+                         'playbooks/conf-new-slave.yaml'])
 
 
 if __name__ == '__main__':
