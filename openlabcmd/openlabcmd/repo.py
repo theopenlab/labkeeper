7236ab847fb80769053e21c17c9dc4a91c53aa31
Yikun Jiang <yikunkero@gmail.com>
2019-05-27 15:11:39 +0800

    Add "openlab repo list" cmd (#101)
    In order to query the list of repos which enable the openlab app or
    allinone app, we propose to add a new cmd to support it.
    
    usage: openlab repo list [-h] [--server SERVER] [--app-id APP_ID]
                              [--app-key APP_KEY]
    
    optional arguments:
      -h, --help         show this help message and exit
      --server SERVER    Specify base server url. Default is github.com
      --app-id APP_ID    Specify the github APP ID, Default is 7102
                         (allinone: 7102, OpenLab: 6778).
      --app-key APP_KEY  Specify the app key file path. Default is
                         /var/lib/zuul/openlab-app-key.pem
    
    closed: https://github.com/theopenlab/openlab/issues/272

---
+++ openlabcmd/openlabcmd/repo.py
@@ -0,0 +1,34 @@
+from openlabcmd import exceptions
+
+
+class Repo(object):
+
+    def __init__(self, server, appid, appkey):
+        # NOTE(yikun): There are two reason we only allow this cmd is executed
+        # in zuul node:
+        # 1. The app installation interface has been completely supported by
+        # zuul github driver (but PyGithub is not supported yet), so we use
+        # it directly to avoid to build the duplicate wheels.
+        # 2. The app key which is usually only existed in Zuul node.
+        try:
+            from zuul.driver.github.githubconnection import GithubConnection
+            from zuul.driver.github import GithubDriver
+        except ImportError:
+            raise exceptions.ClientError(
+                "Error: 'openlab repo list' only can be used in Zuul node.")
+
+        driver = GithubDriver()
+        connection_config = {
+            'server': server,
+            'app_id': appid,
+            'app_key': appkey,
+        }
+        self.conn = GithubConnection(driver, 'github', connection_config)
+        self.conn._authenticateGithubAPI()
+        self.conn._prime_installation_map()
+
+    def list(self):
+        repos = [{"repo": x} for x in self.conn.installation_map]
+        # sort from aA to zZ
+        repos.sort(key=lambda x: x["repo"].lower())
+        return repos
