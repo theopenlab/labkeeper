4bcb8638d167371b0d07b062ddb2a21fa1f30282
bzhao <bzhaojyathousandy@gmail.com>
2020-07-31 15:36:12 +0800

    Update badge.py

--- badge/badge.py
+++ badge/badge.py
@@ -8,38 +8,9 @@ import requests
 
 
 app = Flask(__name__)
-
-
-@app.route('/badge')
-def get_badge():
-    project = request.args.get('project', '')
-    job_name = request.args.get('job_name', '')
-    if not project and not job_name:
-        return Response("Invaild Request")
-    if '/' in project:
-        project = project.replace('/', '%2F')
-    success = True
-    if project:
-        url = "http://status.openlabtesting.org/api/builds?project=%s" % project
-        if job_name:
-            url = url + "&job_name=%s" % job_name
-        status_response = requests.get(url)
-
-        if job_name:
-            status = json.loads(status_response.text)[0]['result']
-            success = True if status == 'SUCCESS' else False
-        else:
-            date_time = None
-            for each in json.loads(status_response.text):
-                if not date_time:
-                    date_time = each['start_time'].split('T')[0]
-                else:
-                    if data_time != each['start_time'].split('T')[0]:
-                        break
-                if each['result'] != 'SUCCESS':
-                    success = False
-    if success:
-        body = """<?xml version="1.0" encoding="UTF-8"?>
+BASE_URL = "http://status.openlabtesting.org/api/builds?"
+RESP_TYPE = {
+    True: """<?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg" width="90" height="20">
   <linearGradient id="a" x2="0" y2="100%">
      <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
@@ -54,9 +25,8 @@ def get_badge():
     <text x="62.5" y="14">passing</text>
   </g>
 </svg>
-        """
-    else:
-        body = '''<?xml version="1.0" encoding="UTF-8"?>
+        """,
+    False: '''<?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg" width="80" height="20">
   <linearGradient id="b" x2="0" y2="100%">
      <stop offset="0" stop-color="#bbb" stop-opacity=".1" />
@@ -78,7 +48,89 @@ def get_badge():
   </g>
 </svg>
 '''
-    response = Response(body, mimetype='image/svg+xml')
+}
+
+
+def get_args(request_args):
+    projects = request_args.getlist('project')
+    job_names = request_args.getlist('job_name')
+    return projects, job_names
+
+
+def genarate_zuul_url(projects, job_names):
+    projects_str_list = []
+    for project in projects:
+        if '/' in project:
+            project = project.replace('/', '%2F')
+        projects_str_list.append("project=%s" % project)
+
+    jobs_str_list = []
+    for job in job_names:
+        jobs_str_list.append("job_name=%s" % job)
+
+    if projects_str_list and not jobs_str_list:
+        url = BASE_URL + "&".join(projects_str_list)
+    elif not projects_str_list and jobs_str_list:
+        url = BASE_URL + "&".join(jobs_str_list)
+    else:
+        url = BASE_URL + "&".join(projects_str_list) + "&" + "&".join(jobs_str_list)
+    return url
+
+
+def check_the_result(url, projects, job_names):
+    status_response = requests.get(url)
+    res = json.loads(status_response.text)
+    if projects and not job_names:
+        projects_set = set(projects)
+        for r in res:
+            if not projects_set:
+                return True
+            if r['project'] in projects_set:
+                if r['result'] != 'SUCCESS':
+                    return False
+                else:
+                    projects_set.remove(r['project'])
+        return True
+
+    elif not projects and job_names:
+        jobs_set = set(job_names)
+        for r in res:
+            if not jobs_set:
+                return True
+            if r['job_name'] in jobs_set:
+                if r['result'] != 'SUCCESS':
+                    return False
+                else:
+                    jobs_set.remove(r['project'])
+        return True
+
+    else:
+        job_names_set = set(job_names)
+        mappings = {}
+        for project in projects:
+            mappings[project] = {}
+        for r in res:
+            if not job_names_set:
+                return True
+            if r['project'] in projects and r['job_name'] in job_names_set:
+                if mappings[r['project']].get(r['job_name'], None):
+                    continue
+                if r['result'] != 'SUCCESS':
+                    return False
+                else:
+                    mappings[r['project']][r['job_name']] = True
+                    job_names_set.remove(r['job_name'])
+        return True
+
+
+@app.route('/badge')
+def get_badge():
+    projects, job_names = get_args(request.args)
+    if (not projects and not job_names) or not projects:
+        return Response("Invaild Request")
+    url = genarate_zuul_url(projects, job_names)
+    success = check_the_result(url, projects, job_names)
+    response = Response(RESP_TYPE[success], mimetype='image/svg+xml')
     response.cache_control.no_cache = True
     response.cache_control.no_store = True
     response.cache_control.private = True
@@ -88,10 +140,10 @@ def get_badge():
     response.add_etag()
     return response
 
+
 if __name__ == '__main__':
     app.run(
       host='0.0.0.0',
-      port= 15000,
+      port=15000,
       debug=True
     )
-
